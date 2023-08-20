fc6ca80c36d77f435f6887e72ce75942df61d84d
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-20 19:23:02 +0200

    .

--- readme.md
+++ readme.md
@@ -59,7 +59,7 @@ Join [Discord](https://discord.com/invite/TVafwaD23d) for more indepth discussio
 
 ## Tasks
 
-- add ability to log multiple variables at once as mentioned [here](https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx081pk/?utm_source=share&utm_medium=web2x&context=3)
+- [add ability to log multiple variables at once as mentioned](https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx081pk/?utm_source=share&utm_medium=web2x&context=3)
 - [add support for multiple values or different ways of formatting](https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx074g9/?utm_source=share&utm_medium=web2x&context=3)
 
 ### ♥️
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,58 @@
 // Macro to print variable name and value only (stripped from release builds)
-#[macro_export]
-macro_rules! log {
-    ($var:expr) => {
-        #[cfg(debug_assertions)]
-        {
-            println!("{}: {:?}", stringify!($var), $var);
+// #[macro_export]
+// macro_rules! log {
+//     ($var:expr) => {
+//         #[cfg(debug_assertions)]
+//         {
+//             println!("{}: {:?}", stringify!($var), $var);
+//         }
+//     };
+// }
+
+macro_rules! log{
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                ::std::eprintln!("{}: {:?}", ::std::stringify!($val), &tmp);
+                tmp
+            }
         }
     };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::log!($val)),+,)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic;
+
+    #[test]
+    fn single_value_test() {
+        let output = panic::catch_unwind(|| {
+            log!(42);
+        })
+        .err()
+        .unwrap()
+        .downcast_ref::<&str>()
+        .unwrap();
+
+        assert_eq!(*output, "42: 42");
+    }
+
+    #[test]
+    fn multiple_values_test() {
+        let output = panic::catch_unwind(|| {
+            log!(42, "hello", true);
+        })
+        .err()
+        .unwrap()
+        .downcast_ref::<&str>()
+        .unwrap();
+
+        // We expect the tuple to have each value logged.
+        assert_eq!(*output, "(42: 42, \"hello\": \"hello\", true: true)");
+    }
 }
