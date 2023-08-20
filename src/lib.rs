d85c598b2b2d768f350db4f8c78023d02f53b303
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-20 20:38:04 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,28 @@
 // got from: https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx0pl1o/
 #[macro_export]
 macro_rules! log {
-    ( $val:expr $(,)? ) => {
-        match $val {
-            tmp => {
-                if ::std::stringify!($val).starts_with("\"") {
-                    // Remove quotes for string literals
-                    ::std::eprintln!("{}", ::std::stringify!($val).trim_matches('\"'));
-                } else {
-                    ::std::eprintln!("{}: {:?}", ::std::stringify!($val), &tmp);
-                }
-                tmp
-            }
+    // Single literal string case
+    ( $val:expr $(,)? ) => {{
+        if ::std::stringify!($val).starts_with("\"") {
+            // Remove quotes for string literals
+            ::std::eprintln!("{}", ::std::stringify!($val).trim_matches('\"'));
+        } else {
+            ::std::eprintln!("{}: {:?}", ::std::stringify!($val), $val);
         }
-    };
-    ( $($val:expr),+ $(,)? ) => {
-        ($($crate::log!($val)),+,)
-    };
+        $val
+    }};
+
+    // Multiple variables case
+    ( $($val:expr),+ $(,)? ) => {{
+        $(
+            $crate::log!($val);
+        )+
+    }};
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use std::io::{self, Read, Write};
 
     #[test]
     fn print_string() {
@@ -35,6 +35,16 @@ mod tests {
     fn print_variable() {
         let animals = vec!["cat", "dog", "horse", "zebra"];
         log!(animals); // -> animals: ["cat", "dog", "horse", "zebra"]
+        assert_eq!("fail", "f");
+    }
+
+    #[test]
+    fn print_multiple_variables() {
+        let animals = vec!["cat", "dog", "horse", "zebra"];
+        let humans = vec!["nikita", "edward"];
+        log!(animals, humans);
+        // -> animals: ["cat", "dog", "horse", "zebra"]
+        // -> humans: ["nikita", "edward"]
         assert_eq!("fail", "f");
     }
 }
