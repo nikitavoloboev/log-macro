fb67666245a355e55bac57b7ee5fa31cf631fe00
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-20 20:55:55 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
-// Macro to print nicely formatted debug info to stderr
-// can print just a string:
-// log!("hello"); // -> hello
-// or variable:
-// let animals = vec!["cat", "dog", "horse", "zebra"];
-// log!(animals); // -> animals: ["cat", "dog", "horse", "zebra"]
-// or multiple variables (each variable on new line):
-// let animals = vec!["cat", "dog", "horse", "zebra"];
-// let humans = vec!["nikita", "edward"];
-// log!(animals, humans); // -> animals: ["cat", "dog", "horse", "zebra"] humans: ["nikita", "edward"]
-// macros are stripped from release builds too
-// credit goes to Icarium-Lifestealer: https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx0pl1o/
+/// Macro to print nicely formatted debug info to stderr
+/// can print just a string:
+/// log!("hello"); // -> hello
+/// or variable:
+/// let animals = vec!["cat", "dog", "horse", "zebra"];
+/// log!(animals); // -> animals: ["cat", "dog", "horse", "zebra"]
+/// or multiple variables (each variable on new line):
+/// let animals = vec!["cat", "dog", "horse", "zebra"];
+/// let humans = vec!["nikita", "edward"];
+/// log!(animals, humans); // -> animals: ["cat", "dog", "horse", "zebra"] humans: ["nikita", "edward"]
+/// macros are stripped from release builds too
+/// credit goes to Icarium-Lifestealer: https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx0pl1o/
 #[macro_export]
 macro_rules! log {
     // Single literal string case
@@ -35,6 +35,8 @@ macro_rules! log {
 mod tests {
     use super::*;
 
+    // TODO: actually test that output is correctly logged
+    // not sure how, https://crates.io/crates/duct pehaps?
     #[test]
     fn print_string() {
         log!("hello"); // -> hello
