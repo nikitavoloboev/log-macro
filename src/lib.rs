e0227f6e18b7856089d8b0d5de9b6eb7b521c1b4
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-20 20:33:06 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 // Macro to print variable name and value only (stripped from release builds)
 // got from: https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx0pl1o/
 #[macro_export]
-macro_rules! log{
-    ($val:expr $(,)?) => {
-        // Use of `match` here is intentional because it affects the lifetimes
-        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+macro_rules! log {
+    ( $val:expr $(,)? ) => {
         match $val {
             tmp => {
-                ::std::eprintln!("{}: {:?}", ::std::stringify!($val), &tmp);
+                if ::std::stringify!($val).starts_with("\"") {
+                    // Remove quotes for string literals
+                    ::std::eprintln!("{}", ::std::stringify!($val).trim_matches('\"'));
+                } else {
+                    ::std::eprintln!("{}: {:?}", ::std::stringify!($val), &tmp);
+                }
                 tmp
             }
         }
     };
-    ($($val:expr),+ $(,)?) => {
+    ( $($val:expr),+ $(,)? ) => {
         ($($crate::log!($val)),+,)
     };
 }
@@ -20,10 +23,18 @@ macro_rules! log{
 #[cfg(test)]
 mod tests {
     use super::*;
+    // use std::io::{self, Read, Write};
 
     #[test]
     fn print_string() {
-        log!("testing");
-        assert_eq!("fail", "test");
+        log!("hello"); // -> hello
+        assert_eq!("hello", "hello.");
+    }
+
+    #[test]
+    fn print_variable() {
+        let animals = vec!["cat", "dog", "horse", "zebra"];
+        log!(animals); // -> animals: ["cat", "dog", "horse", "zebra"]
+        assert_eq!("fail", "f");
     }
 }
