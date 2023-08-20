d3c1d61d452e47572d54fb5ff9f443ece3bd91ec
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-08-20 20:17:58 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
 // Macro to print variable name and value only (stripped from release builds)
-// #[macro_export]
-// macro_rules! log {
-//     ($var:expr) => {
-//         #[cfg(debug_assertions)]
-//         {
-//             println!("{}: {:?}", stringify!($var), $var);
-//         }
-//     };
-// }
-
+// got from: https://www.reddit.com/r/rust/comments/15wd5u6/comment/jx0pl1o/
+#[macro_export]
 macro_rules! log{
     ($val:expr $(,)?) => {
         // Use of `match` here is intentional because it affects the lifetimes
@@ -27,32 +19,11 @@ macro_rules! log{
 
 #[cfg(test)]
 mod tests {
-    use std::panic;
+    use super::*;
 
     #[test]
-    fn single_value_test() {
-        let output = panic::catch_unwind(|| {
-            log!(42);
-        })
-        .err()
-        .unwrap()
-        .downcast_ref::<&str>()
-        .unwrap();
-
-        assert_eq!(*output, "42: 42");
-    }
-
-    #[test]
-    fn multiple_values_test() {
-        let output = panic::catch_unwind(|| {
-            log!(42, "hello", true);
-        })
-        .err()
-        .unwrap()
-        .downcast_ref::<&str>()
-        .unwrap();
-
-        // We expect the tuple to have each value logged.
-        assert_eq!(*output, "(42: 42, \"hello\": \"hello\", true: true)");
+    fn print_string() {
+        log!("testing");
+        assert_eq!("fail", "test");
     }
 }
